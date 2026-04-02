582c7b56c82acf2e99ab7cd85a96bebdf4ee109e
jl9904 <lopezjaden004@gmail.com>
2026-04-01 23:31:32 -0500

    added pick and randomitem pug file

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var lighthouseRouter = require('./routes/lighthouse');
-var gridRouter = require('./routes/grid');  
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');  
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/lighthouse', lighthouseRouter);
 app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
